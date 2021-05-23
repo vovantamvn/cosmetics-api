365c3a09a0882c76f8d2b83ac4f9a1dc39c37649
tamvv <vovantam.dev@gmail.com>
2021-05-23 22:27:32 +0700

    Refactor exception

--- src/main/java/com/app/cosmetics/api/exception/InvalidRequestException.java
+++ src/main/java/com/app/cosmetics/api/exception/InvalidRequestException.java
@@ -2,7 +2,7 @@ package com.app.cosmetics.api.exception;
 
 import org.springframework.validation.Errors;
 
-public class InvalidRequestException extends RuntimeException {
+public final class InvalidRequestException extends RuntimeException {
     private final Errors errors;
 
     public InvalidRequestException(Errors errors) {
--- src/main/java/com/app/cosmetics/api/exception/NotFoundException.java
+++ src/main/java/com/app/cosmetics/api/exception/NotFoundException.java
@@ -2,7 +2,7 @@ package com.app.cosmetics.api.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class NotFoundException extends MyException {
+public final class NotFoundException extends MyException {
 
     public NotFoundException() {
         super(HttpStatus.NOT_FOUND, "Not Found Exception");

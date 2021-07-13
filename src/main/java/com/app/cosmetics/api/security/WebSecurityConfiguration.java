684cdcd3052026a7a2e662217cebaeeb3fd898fb
tamvv <vovantam.dev@gmail.com>
2021-07-13 19:46:56 +0700

    Update upload image API

--- src/main/java/com/app/cosmetics/api/security/WebSecurityConfiguration.java
+++ src/main/java/com/app/cosmetics/api/security/WebSecurityConfiguration.java
@@ -33,7 +33,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/login", "/accounts", "/orders").permitAll()
+                .antMatchers(HttpMethod.POST,"/login", "/accounts", "/orders", "/uploadFile").permitAll()
                 .antMatchers(HttpMethod.GET, "/**").permitAll()
                 .antMatchers("/comments/**").permitAll()
                 .anyRequest().authenticated()
